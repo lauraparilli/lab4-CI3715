454cb3eceb96f63a87da829cf56a589e780ca793
laura <106710365+lauraparilli@users.noreply.github.com>
2024-11-08 14:33:47 -0400

    Cambio en archivos despues de usar formateador de codigo (#2)
    * Update main.yml
    
    * Update fib.py
    
    * Update test_fib.py

--- fib.py
+++ fib.py
@@ -5,11 +5,12 @@ El número cero en la secuencia de Fibonacci es 0. El primer número es 1
 Los números negativos no son aceptados
 """
 
+
 def fibonacci(position):
-  if(position < 0):
-    raise ValueError("Invalid input")
-  if(position == 1 or position == 2):
-    return 1
-  if(position == 0):
+    if position < 0:
+        raise ValueError("Invalid input")
+    if position == 1 or position == 2:
+        return 1
+    if position == 0:
         return 0
-  return fibonacci(position - 1) + fibonacci(position - 2)
+    return fibonacci(position - 1) + fibonacci(position - 2)
